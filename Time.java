4866b1c04d491b18c12610fe6893beadb451618e
titlemarek <tiger.ok123@gmail.com>
2019-12-04 00:55:35 +0700

    close#4 - ทำหน้าUI อิอิ

---
+++ Time.java
@@ -0,0 +1,35 @@
+package com.okta.springbootvue.entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="TIME")
+public class Time {
+    @Id
+    @SequenceGenerator(name="TIME_SEQ",sequenceName="TIME_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="TIME_SEQ")
+    @Column(name="TIME_ID",unique = true, nullable = true)
+    private @NonNull Long id;
+
+    private @NonNull String name;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    
+    private Collection<Showtime> showtime;
+}
